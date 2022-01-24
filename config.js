cb741fe5502ba08877049f685734929df8b2e14b
risseh <94009840+risseh@users.noreply.github.com>
2022-01-24 22:03:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['6283136262784', '6283136262784'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['6283168469560] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
